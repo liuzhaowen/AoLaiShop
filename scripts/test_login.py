8797cd4090e0e4c56fcf44734aa3adfc53c984e9
liuzhaowen <liuzhaowen007@163.com>
2019-06-13 16:40:26 +0800

    002登录

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,6 +1,7 @@
 import time
 
 from base.base_driver import init_driver
+from page.page import Page
 
 
 class TestLogin:
@@ -8,7 +9,7 @@ class TestLogin:
 
     def setup(self):
         self.driver = init_driver()
-
+        self.page = Page(self.driver)
 
     def teardown(self):
         time.sleep(3)
@@ -16,4 +17,16 @@ class TestLogin:
 
 
     def test_login(self):
-        assert 1
+
+        # 在home页点击me
+        self.page.home.click_me()
+        # 在register页点击 已有账号去登录
+        self.page.register.click_to_login()
+        # 在login页输入用户名
+        self.page.login.input_username("itheima_test")
+        # 在login页输入密码
+        self.page.login.input_password("itheima")
+        # 在login页点击登录按钮
+        self.page.login.click_login_btn()
+        # 在me 页获取个人信息页断言
+        assert "itheima_test" == self.page.me.get_nickname()
